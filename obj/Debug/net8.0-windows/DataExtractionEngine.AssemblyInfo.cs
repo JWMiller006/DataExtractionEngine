603ca3e3eb0ffaaa541b5c17e81ea7d5a048ad5c
J Miller <jwmiller006@outlook.com>
2024-01-17 08:03:33 -0500

    info files

--- obj/Debug/net8.0-windows/DataExtractionEngine.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/DataExtractionEngine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataExtractionEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39a1bb236945d5bc59dec928dd28d430d98d6863")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataExtractionEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataExtractionEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
