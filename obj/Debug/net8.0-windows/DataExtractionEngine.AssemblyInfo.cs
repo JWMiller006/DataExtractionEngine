b9db638fe0378509c4c782004ae9bf7cd95efafc
JWMiller006 <jwmiller006@outlook.com>
2024-01-31 09:55:00 -0500

    update

--- obj/Debug/net8.0-windows/DataExtractionEngine.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/DataExtractionEngine.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("J Miller 2024")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66b8fa666d6fd23a8802f1e9738128416ef15034")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4402e64c0274c489b784f43b86bacfd7a629819")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataExtractionEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataExtractionEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
