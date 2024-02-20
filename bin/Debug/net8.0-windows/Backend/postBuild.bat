820b33ac44d89bb148fd94543e07c6efee8e42a3
JWMiller006 <jwmiller006@outlook.com>
2024-02-20 10:32:06 -0500

    add postBuild tasks

---
+++ bin/Debug/net8.0-windows/Backend/postBuild.bat
@@ -0,0 +1,7 @@
+﻿echo    > AppData.json
+echo  { >> AppData.json
+echo   "AppName": "DataExtractionEngine", >> AppData.json
+echo   "AppVersion": "1.0.1.2", >> AppData.json
+echo   "AppDescription": "Extract data from output files for a specified project", >> AppData.json
+echo   "AppPublisher": "MillerInc" >> AppData.json
+echo } >> AppData.json
