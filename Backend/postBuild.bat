597f149866a3fac2ad56aaa49e2f4a506a3f466e
JWMiller006 <jwmiller006@outlook.com>
2024-02-23 21:19:04 -0500

    update model

--- Backend/postBuild.bat
+++ Backend/postBuild.bat
@@ -1,7 +1,8 @@
 ﻿echo    > AppData.json
-echo  { >> AppData.json
+echo { >> AppData.json
 echo   "AppName": "DataExtractionEngine", >> AppData.json
 echo   "AppVersion": "1.0.1.3", >> AppData.json
 echo   "AppDescription": "Extract data from output files for a specified project", >> AppData.json
 echo   "AppPublisher": "MillerInc" >> AppData.json
+echo   "SupportsMillerInc": true >> AppData.json
 echo } >> AppData.json
--- bin/Debug/net8.0-windows/Backend/postBuild.bat
+++ bin/Debug/net8.0-windows/Backend/postBuild.bat
@@ -4,4 +4,5 @@ echo   "AppName": "DataExtractionEngine", >> AppData.json
 echo   "AppVersion": "1.0.1.3", >> AppData.json
 echo   "AppDescription": "Extract data from output files for a specified project", >> AppData.json
 echo   "AppPublisher": "MillerInc" >> AppData.json
+echo   "SupportsMillerInc": true >> AppData.json
 echo } >> AppData.json
